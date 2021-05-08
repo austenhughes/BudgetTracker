7b0711548380944772c761f1553c32146fffeb5e
Austn Hughes <austen.hughes.91@gmail.com>
2021-05-08 15:35:44 -0600

    pre heroku commit

--- public/db.js
+++ public/db.js
@@ -4,12 +4,7 @@ let budgetVersion;
 const request = indexedDB.open('BudgetDB', budgetVersion || 1);
 
 request.onupgradeneeded = function (e) {
-  
-    const { oldVersion } = e;
-    const newVersion = e.newVersion || db.version;
-  
     db = e.target.result;
-  
     if (db.objectStoreNames.length === 0) {
       db.createObjectStore('BudgetStore', { autoIncrement: true });
     }
@@ -19,14 +14,10 @@ request.onerror = function (e) {
     console.log(`error ${e.target.errorCode}`);
   };
 
-  function checkDatabase() {
-
+function checkDatabase() {
     let transaction = db.transaction(['BudgetStore'], 'readwrite');
-  
     const store = transaction.objectStore('BudgetStore');
-  
     const getAll = store.getAll();
-  
     getAll.onsuccess = function () {
      if (getAll.result.length > 0) {
         fetch('/api/transaction/bulk', {
@@ -38,16 +29,11 @@ request.onerror = function (e) {
           },
         })
           .then((response) => response.json())
-          .then((res) => {
-            
-            if (res.length !== 0) {
-         
-              transaction = db.transaction(['BudgetStore'], 'readwrite');
-  
-              const currentStore = transaction.objectStore('BudgetStore');
- 
+          .then((res) => {          
+            if (res.length !== 0) {        
+              transaction = db.transaction(['BudgetStore'], 'readwrite');  
+              const currentStore = transaction.objectStore('BudgetStore'); 
               currentStore.clear();
-
             }
           });
       }
@@ -55,25 +41,15 @@ request.onerror = function (e) {
   }
   
 request.onsuccess = function (e) {
-    // console.log('success');
     db = e.target.result;
-  
-    // Check if app is online before reading from db
     if (navigator.onLine) {
-    //   console.log('Backend online! 🗄️');
       checkDatabase();
     }
   };
   
 const saveRecord = (record) => {
-    // console.log('Save record invoked');
-    // Create a transaction on the BudgetStore db with readwrite access
     const transaction = db.transaction(['BudgetStore'], 'readwrite');
-  
-    // Access your BudgetStore object store
     const store = transaction.objectStore('BudgetStore');
-  
-    // Add record to your store with add method.
     store.add(record);
   };
 
