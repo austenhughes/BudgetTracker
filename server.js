104111ad4b153b7db2ebd7bca800fbb01b7840d5
Austn Hughes <austen.hughes.91@gmail.com>
2021-05-08 18:13:37 -0600

    working on heroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-  "mongodb://localhost/agile-garden-31275", {
+  "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
